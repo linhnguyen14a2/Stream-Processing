52ea221b7faba1dd46ffa93743e1e9ced78b69d8
L33t14 <khanhlinh14a2@gmail.com>
2020-02-27 14:51:49 -0500

    service layer

---
+++ ITestService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WebAPIapp.Service
+{
+    public interface ITestService
+    {
+        Task<int> StreamProcessingScore(string input);
+    }
+}
---
+++ TestService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIapp.Service
+{
+    public class TestService : ITestService
+    {
+        public async Task<int> StreamProcessingScore(string input)
+        {
+            int totalScore = 0;
+            int depth = 0;
+            bool inGarbage = false;
+            bool ignore = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (ignore)
+                {
+                    ignore = false;
+                    continue;
+                }
+                else if(input[i] == '!') 
+                    { ignore = true; }
+
+                else if (inGarbage)
+                {
+                    if (input[i] == '>') 
+                    { inGarbage = false; }
+                }
+
+                else if (input[i] == '<') 
+                    { inGarbage = true; }
+
+                else if(input[i]=='{')
+                {
+                    depth += 1;
+                    totalScore += depth;
+                }
+                else if(input[i] == '}')
+                {
+                    depth -= 1;
+                }
+
+                
+            }
+
+            return totalScore;
+        }
+    }
+}
